a58e6bd65833c9d4d3b71b341a83138842fc02b3
Nirkan <39731253+Nirkan@users.noreply.github.com>
2023-12-28 14:03:42 +0000

    Few Modifications and additions

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
-JOBS = [
-  {
-    'id':'1',
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary':'Rs. 10,00,000'
-  },
-  {
-    'id':'2',
-    'title': 'Data Analyst',
-    'location': 'Delhi, India',
-    'salary':'Rs. 15,00,000'
-  },
-  {
-    'id':'3',
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-  },
-  {
-    'id':'4',
-    'title': 'Backend Engineer',
-    'location': 'Chicago, USA',
-    'salary':'$150,000'
-  }
-]
 
 @app.route("/")
 def Hello_NextStep():
   return render_template("home.html", 
-                         jobs=JOBS,
+                         jobs=jobs,
                         company_name="NextStep")
 
 
@@ -39,6 +14,14 @@ def Hello_NextStep():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = show_job_from_id(job)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,27 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text 
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine()    # Need to fill the ()  - Incomplete 
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row))
+    return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(SELECT * FROM jobs WHERE id = :val), val=id))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+      
+    
